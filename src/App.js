0b1f198523abfa20b4ee00e2928f74839599ac99
shivishiv7 <shivishiv7@gmail.com>
2021-07-12 11:07:22 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Amplify, {Storage } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 
-function App() {
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+function Upload() {
+  const fileInput = React.useRef();
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    let newArr = fileInput.current.files;
+  
+    for (let i = 0; i < newArr.length; i++) {
+      handleUpload(newArr[i],i);
+    }
+  };
+
+  const handleUpload =  (file,i) => {
+    let newFileName = file.name.replace(/\..+$/, "");
+    
+    try {
+     Storage.put(newFileName, file, {
+        progressCallback(progress) {
+            console.log(`Uploaded: ${Math.round((progress.loaded/progress.total)*100)}`);
+          // fruits.set(newFileName,Math.round((progress.loaded/progress.total)*100));
+            let t   = Math.round((progress.loaded/progress.total)*100);
+            //map.set(newFileName,track)
+            console.log(t,i,newFileName)
+        
+          },
+    }).then(e=>{
+     // console.log(e)
+    });
+    } catch (error) {
+      console.log('Error uploading file: ', error);
+    }
+    // const ReactS3Client = new S3(config);
+    // ReactS3Client.uploadFile(file, newFileName).then((data) => {
+    //   if (data.status === 204) {
+    //     console.log("success");
+    //   } else {
+    //     console.log("fail");
+    //   }
+    // });
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form className='upload-steps' onSubmit={handleClick}>
+        <label>
+          Upload file:
+          <input type='file' multiple ref={fileInput} />
+        </label>
+        <br />
+
+        <button type='submit'>Upload</button>
+      </form>
+    </>
   );
 }
-
-export default App;
+export default withAuthenticator(Upload);
+//export default Upload;
